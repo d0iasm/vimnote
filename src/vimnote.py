f9211e4831c031b6f60c284bc93b4b5c90a4e5a1
Asami Doi <doiasami1219@gmail.com>
2017-05-05 17:20:59 +0900

    modify: get evernote data using python api

--- src/vimnote.py
+++ src/vimnote.py
@@ -1,6 +1,13 @@
-import os
-from os.path import join, dirname
+from evernote.api.client import EvernoteClient
 
-if __name__ == '__main__':
-    path = join(dirname(__file__), '.env')
-    print(path)
+
+dev_token = "put your dev token here"
+client = EvernoteClient(token=dev_token)
+userStore = client.get_user_store()
+user = userStore.getUser()
+print(user.username)
+
+noteStore = client.get_note_store()
+notebooks = noteStore.listNotebooks()
+for n in notebooks:
+    print(n.name)
