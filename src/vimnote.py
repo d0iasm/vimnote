13514795d7004472f261d81d46bc186103d9b6e9
Asami Doi <doiasami1219@gmail.com>
2017-05-18 23:10:34 +0900

    modify: not use setting

--- src/vimnote.py
+++ src/vimnote.py
@@ -5,11 +5,13 @@ from datetime import datetime
 from evernote.api.client import EvernoteClient
 import evernote.edam.type.ttypes as Types
 
-from setting import Setting
+# from setting import Setting
 
 
 class Vimnote(object):
     _instance = None
+    _client = None
+    _dev_token = None
 
     def __init__(self, *args, **keys):
         pass
@@ -21,8 +23,16 @@ class Vimnote(object):
 
         return self._instance
 
+    def getClient(self):
+        if self._client is None:
+            self._dev_token = vim.eval("evernote_dev_token")
+            self._client = EvernoteClient(token=self._dev_token)
+
+        return self._client
+
     def sendNote(self):
-        client = Setting.getClient()
+        client = getClient()
+        # client = Setting.getClient()
         noteStore = client.get_note_store()
         note = Types.Note()
         note.title = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
