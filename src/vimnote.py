691a5dc13c896d33bb15d826e8cd62823da47901
Asami Doi <doiasami1219@gmail.com>
2017-05-18 22:02:49 +0900

    modify: get current buffer data from vim

--- src/vimnote.py
+++ src/vimnote.py
@@ -21,16 +21,15 @@ class Vimnote(object):
 
         return self._instance
 
-    def sendNote(self, title='', content):
+    def sendNote(self):
         client = Setting.getClient()
         noteStore = client.get_note_store()
         note = Types.Note()
-        if not title:
-            note.title = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        else:
-            note.title = title
+        note.title = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
         note.content += '<en-note>'
-        note.content += content
+        for buffer in vim.buffers:
+            for i in buffer:
+                note.content += i
         note.content += '</en-note>'
         note = noteStore.createNote(note)
